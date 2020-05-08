7761eccfeaa114cb2b6069eeeaf933a849c42cc3
diyar-2020 <dyartymwry1@gmail.com>
2020-05-08 22:38:13 +0430

    new game 4

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1588961237,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"bg_logo.png",
+		"just_txt-sheet0.png",
+		"maral_love_txt-sheet0.png",
+		"heart_ward-sheet0.png",
+		"jump-sheet0.png",
+		"left_b-sheet0.png",
+		"star-sheet0.png",
+		"fire_b-sheet0.png",
+		"bg_button-sheet0.png",
+		"bg_game.png",
+		"heart_g-sheet0.png",
+		"ward_solid_delet_player-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
